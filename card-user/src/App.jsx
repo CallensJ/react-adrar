51e7383239f88a5e88a817b1b158f644efc92787
CallensJ <callens.johan@protonmail.com>
2025-01-27 14:22:42 +0100

    map method

--- card-user/src/App.jsx
+++ card-user/src/App.jsx
@@ -1,22 +1,47 @@
 // import { useState } from 'react'
 
-import './App.css'
-import Card from './Card'
+import "./App.css";
+import Card from "./Card";
+
+const users = [
+  {
+    avatar: "https://randomuser.me/api/portraits/men/1.jpg",
+    pseudo: "anonymous",
+    email: "anon@gmail.fr",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id lorem metus. "
+  },
+  {
+    avatar: "https://randomuser.me/api/portraits/women/2.jpg",
+    pseudo: "batman",
+    email: "batman@gmail.fr",
+    description: "i'm Batman"
+  },
+  {
+    avatar: "https://randomuser.me/api/portraits/men/3.jpg",
+    pseudo: "superman",
+    email: "superman@gmail.fr",
+    description: "Supermannnnn ta ta daaaaaa"
+  }
+];
+
 
 function App() {
-
-
   return (
     <>
-    <h1>Liste des Utilisateurs</h1> 
-     <Card
-    avatar="./assets/avatar.jpeg"
-    pseudo="anonymous" 
-    email="anon@gmail.fr" 
-    description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse nisl erat, porttitor ac massa tristique, convallis tempus nisi. Sed quis ipsum condimentum, condimentum lectus"
-/>
+      <h1>Liste des Utilisateurs</h1>
+      <main>
+      {users.map((user, index) => (
+        <Card
+          key={index}
+          avatar={user.avatar}
+          pseudo={user.pseudo}
+          email={user.email}
+          description={user.description}
+        />
+      ))}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
