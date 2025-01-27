341fa10ec18d32d242071927c770164cdf14cf7a
CallensJ <callens.johan@protonmail.com>
2025-01-27 14:07:44 +0100

    update

---
+++ card-user/src/Card.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+import './Card.css';
+import PropTypes from 'prop-types';
+
+
+const Card = ({ avatar, pseudo, email, description }) => {
+  return (
+    <article className="card">
+      <div className="card-content">
+        <img
+          src={avatar}
+          alt="Avatar"
+          className="avatar"
+        />
+        <h2 className="pseudo">{pseudo}</h2>
+        <h3 className="email">{email}</h3>
+        <p className="description">{description}</p>
+      </div>
+    </article>
+  );
+};
+Card.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    pseudo: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+  };
+  
+export default Card;
