9184f37b237ff373a93ea3afec619fa2aad6372c
CallensJ <callens.johan@protonmail.com>
2025-01-29 11:42:56 +0100

    added sexe on users

--- card-user/src/App.jsx
+++ card-user/src/App.jsx
@@ -8,19 +8,22 @@ const users = [
     avatar: "https://randomuser.me/api/portraits/men/1.jpg",
     pseudo: "anonymous",
     email: "anon@gmail.fr",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id lorem metus. "
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque id lorem metus. ",
+    sexe: "masculin"
   },
   {
     avatar: "https://randomuser.me/api/portraits/women/2.jpg",
     pseudo: "batman",
     email: "batman@gmail.fr",
-    description: "i'm Batman"
+    description: "i'm Batman",
+    sexe: "masculin"
   },
   {
     avatar: "https://randomuser.me/api/portraits/men/3.jpg",
-    pseudo: "superman",
-    email: "superman@gmail.fr",
-    description: "Supermannnnn ta ta daaaaaa"
+    pseudo: "supergirl",
+    email: "supergirl@gmail.fr",
+    description: "Supergirllllll ta ta daaaaaa",
+    sexe: "feminin"
   }
 ];
 
@@ -28,18 +31,31 @@ const users = [
 function App() {
   return (
     <>
-      <h1>Liste des Utilisateurs</h1>
-      <main>
-      {users.map((user, index) => (
-        <Card
-          key={index}
-          avatar={user.avatar}
-          pseudo={user.pseudo}
-          email={user.email}
-          description={user.description}
-        />
-      ))}
-      </main>
+  <main>
+      {users.length > 0 && <p>Il y a {users.length} utilisateur(s) inscrit(s) !</p>}
+      {users.length > 0 ? (
+        <>
+          <h1>Liste des Utilisateurs</h1>
+          {users.map((user, index) => (
+            <div
+            key={index}
+            style={{ backgroundColor: user.sexe === "masculin" ? "lightblue" : "pink", padding: "10px"}}
+            >
+
+              <Card
+                
+                avatar={user.avatar}
+                pseudo={user.pseudo}
+                email={user.email}
+                description={user.description}
+              />
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Aucun utilisateur</p>
+      )}
+    </main>
     </>
   );
 }
--- card-user/src/Card.jsx
+++ card-user/src/Card.jsx
@@ -3,7 +3,7 @@ import './Card.css';
 import PropTypes from 'prop-types';
 
 
-const Card = ({ avatar, pseudo, email, description }) => {
+const Card = ({ avatar, pseudo, email, description, sexe }) => {
   return (
     <article className="card">
       <div className="card-content">
@@ -15,6 +15,7 @@ const Card = ({ avatar, pseudo, email, description }) => {
         <h2 className="pseudo">{pseudo}</h2>
         <h3 className="email">{email}</h3>
         <p className="description">{description}</p>
+        <p className="sexe">{sexe}</p>
       </div>
     </article>
   );
@@ -24,6 +25,7 @@ Card.propTypes = {
     pseudo: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
+    sexe: PropTypes.string.isRequired
   };
   
 export default Card;
